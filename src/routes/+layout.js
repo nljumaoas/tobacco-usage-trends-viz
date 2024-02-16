dbdc73f5f6605a9613fc37fa69889a8b3a0ae53d
Nicholas Jumaoas <nljumaoas@gmail.com>
2024-02-16 01:18:11 -0800

    idk what is happening rn ngl

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,2 +1,24 @@
 // This can be false if you're using a fallback (i.e. SPA mode)
 export const prerender = true;
+
+import * as d3 from 'd3';
+import * as topojson from 'topojson';
+
+const width = 928;
+const marginTop = 46;
+const height = width / 2 + marginTop;
+
+if (typeof window !== 'undefined') {
+    // Code that depends on the DOM, like D3's select method
+    const svg = d3.select('body').append('svg').attr('width', width).attr('height', height);
+
+    const projection = d3.geoEqualEarth().fitExtent([[2, marginTop + 2], [width - 2, height]], {type: "Sphere"});
+    const path = d3.geoPath()
+
+    const g = svg.append('g');
+
+    d3.json('world.json').then(data => {
+        const countries = topojson.feature(data, data.objects.countries);
+        g.selectAll('path').data(countries.features).enter().append('path').attr('class', 'country').attr(d, path)
+    });
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,120 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import * as d3 from 'd3';
+
+	export let todo_category = [];
+	
+	let hovered = -1; 
+
+	let recorded_mouse_position = {
+		x: 0, y: 0
+	};
+
+	let arcGenerator = d3.arc()
+		.innerRadius(10)
+		.outerRadius(100)
+		.padAngle(.02)
+		.cornerRadius(4);
+	let pieAngleGenerator = d3.pie().value( d => d[1] );
+	let arc_data = []
+	const arc_color = d3.scaleLinear()
+		.range(["#faffd1", "#db921d", "#b86a04", "#a65d29" ,"#6e3003"]);
+
+	$: {
+		// interactive data here
+		// input data grouped by category
+		// looks like [[category, count], [category, count], [category, count]]
+		let todo_count_by_category = d3.rollups(
+			todo_category, 
+			v => v.length, 
+			d => d.category
+		);
+		
+		arc_data = pieAngleGenerator(todo_count_by_category);
+		console.log(arc_data)
+		console.log(JSON.stringify(todo_category));
+	}
+
+
+</script>
+
+<div class="visualization">
+	<svg width="500" height="500">
+		<g transform="translate(250, 120)">
+			{#each arc_data as data, index}
+			<path 
+				d={arcGenerator({
+					startAngle: data.startAngle,
+					endAngle: data.endAngle
+				})}
+				fill={index === hovered ? "brown": arc_color(index)}
+				on:mouseover={(event) => { 
+					hovered = index; 
+					recorded_mouse_position = {
+							x: event.pageX,
+							y: event.pageY
+						}
+					}}
+				on:mouseout={(event) => { hovered = -1; }}
+			/>
+			{/each}
+
+		</g>
+	</svg>
+	<div
+		class={hovered === -1 ? "tooltip-hidden": "tooltip-visible"}
+		style="left: {recorded_mouse_position.x + 40}px; top: {recorded_mouse_position.y + 40}px"	
+	>
+		{#if hovered !== -1}
+		    There { arc_data[hovered].data[1]===1 ? "is" : "are" } { arc_data[hovered].data[1] } record{ arc_data[hovered].data[1]===1 ? "" : "s" } 
+		where you have "{ arc_data[hovered].data[0] }" todo items.
+
+		{/if}
+	</div>
+
+</div>
+
+
+
+<main>
+    <h1>Hello World!</h1>
+    <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+    <div class="visualization">
+        <svg width="500" height="500">
+            <g transform="translate(250, 120)">
+                <!-- Place for Pie -->
+            </g>
+        </svg>
+    </div>
+    
+</main>
+
+<style>
+	.visualization {
+		font: 25px sans-serif;
+		margin: auto;
+		margin-top: 1px;
+		text-align: middle;
+	}
+
+	/* dynamic classes for the tooltip */
+	.tooltip-hidden {
+		visibility: hidden;
+		font-family: "Nunito", sans-serif;
+		width: 200px;
+		position: absolute;
+	}
+
+	.tooltip-visible {
+		font: 25px sans-serif;
+		font-family: "Nunito", sans-serif;
+		visibility: visible;
+		background-color: #f0dba8;
+		border-radius: 10px;
+		width: 200px;
+		color: black;
+		position: absolute;
+		padding: 10px;
+	}
+</style>
+
